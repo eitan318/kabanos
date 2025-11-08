debb2f667a45df3bc1f9f3ddc9003149159a8672
eitan318 <eitanforschool08@gmail..com>
2025-11-08 23:23:52 +0200

    Successfully read partition table from stage2

--- src/bootloader/stage2/fat.c
+++ src/bootloader/stage2/fat.c
@@ -5,43 +5,47 @@
 #define SECTOR_SIZE 512
 
 typedef struct __attribute__((packed)) {
-  uint8_t boot_jump_instruction[3];
-  uint8_t oem_identifier[8];
-  uint16_t bytes_per_sector;
-  uint8_t sectors_per_cluster;
-  uint16_t reserved_sectors;
-  uint8_t fat_count;
-  uint16_t dir_entry_count;
-  uint16_t total_sectors;
-  uint8_t media_descriptor_type;
-  uint16_t sectors_per_fat;
-  uint16_t sectors_per_track;
-  uint16_t heads;
-  uint32_t hidden_sectors;
-  uint32_t large_sector_count;
+  uint8_t boot_flag;      // 0x80 = bootable, 0x00 = non-bootable
+  uint8_t chs_start[3];   // CHS address of first sector (ignored by LBA)
+  uint8_t partition_type; // e.g., 0x0B = FAT32 CHS, 0x0C = FAT32 LBA
+  uint8_t chs_end[3];     // CHS address of last sector
+  uint32_t lba_start;     // LBA of first sector of partition
+  uint32_t total_sectors; // number of sectors in partition
+} MBRPartitionEntry;
 
-  // extended boot record
-  uint8_t drive_number;
-  uint8_t _reserved;
-  uint8_t signature;
-  uint32_t volume_id;       // serial number, value doesn't matter
-  uint8_t volume_label[11]; // 11 bytes, padded with spaces
-  uint8_t system_id[8];
-
-} FatBootSector;
+typedef struct __attribute__((packed)) {
+  uint8_t boot_code[446];
+  MBRPartitionEntry partitions[4];
+  uint16_t boot_signature; // should be 0xAA55
+} MBR;
 
 union {
-  FatBootSector boot_sector;
-  uint8_t boot_sector_bytes[SECTOR_SIZE];
-} g_boot_sector;
+  MBR mbr;
+  uint8_t bytes[SECTOR_SIZE];
+} g_mbr;
 
 bool read_boot_sector(DiskParams *disk) {
-  return disk_read_sectors(disk, 0, 1, g_boot_sector.boot_sector_bytes);
+  return disk_read_sectors(disk, 0, 1, g_mbr.bytes);
 }
-
 void check(DiskParams *disk) {
   if (!read_boot_sector(disk)) {
-    debugf("Failed to read boot sector\n");
+    debugf("Failed to read MBR\n");
     return;
+  }
+
+  if (g_mbr.mbr.boot_signature != 0xAA55) {
+    debugf("Invalid MBR signature: 0x%04X\n", g_mbr.mbr.boot_signature);
+    return;
+  }
+
+  debugf("MBR Partition Table:\n");
+  debugf("Idx | Boot | Type |   LBA Start  | Total Sectors\n");
+  debugf("-----------------------------------------------\n");
+
+  for (int i = 0; i < 4; i++) {
+    MBRPartitionEntry *p = &g_mbr.mbr.partitions[i];
+
+    debugf("%d |  0x%X | 0x%X | %u | %u\n", i, p->boot_flag, p->partition_type,
+           p->lba_start, p->total_sectors);
   }
 }
