de7541c1567ea3b858dceaef344670d3788155d1
Xavier Claessens <116307116+sulieliya@users.noreply.github.com>
2025-11-20 14:43:32 +0200

    write keyboard initialization

--- src/kernel/keyboard_driver/keyboard_driver.c
+++ src/kernel/keyboard_driver/keyboard_driver.c
@@ -1,6 +1,11 @@
+#include "keyboard_driver/keyboard_driver.h"
+#include "arch/i686/io.h"
+#include "arch/i686/isr/isr.h"
+#include "include/stdio.h"
+#include "utils/queue.h"
+#include "arch/i686/pic.h"  
 
-
-
+Queue keyboard_queue;
 
 // Scancode -> ASCII mapping (partial, US layout)
 static char scancode_to_ascii[128] = {
@@ -10,5 +15,15 @@ static char scancode_to_ascii[128] = {
     'j', 'k', 'l',  ';',  '\'', '`', 0,   '\\', 'z', 'x', 'c', 'v',
     'b', 'n', 'm',  ',',  '.',  '/', 0,   '*',  0,   ' ', 0};
 	
-	
+void kbd_init() {
+    queue_init(&keyboard_queue);
+
+    // NOTE: pic_init() is called in hal_init(), not here
+    
+    // Register keyboard interrupt handler
+    i686_isr_handler_register(KBD_INT, keyboard_isr_handler);
+    
+    // Enable keyboard interrupt (IRQ1)
+    pic_unmask_irq(KBD_IRQ);
+}
 	
