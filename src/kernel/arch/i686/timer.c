82c5663dee8bc6c2a6f885a0402a929d923cb7c3
Xavier Claessens <116307116+sulieliya@users.noreply.github.com>
2026-01-29 15:02:23 +0200

    edit define name in timer.c

--- src/kernel/arch/i686/timer.c
+++ src/kernel/arch/i686/timer.c
@@ -3,7 +3,7 @@
 #include "sched/sched.h"
 
 #define TIMER_IRQ 0
-#define TIMER_VECTOR 32
+#define TIMER_INT 0x20
 
 static void timer_isr(struct regs *r) {
   hal_irq_send_eoi(TIMER_IRQ); // Works only with eoi before
@@ -11,6 +11,6 @@ static void timer_isr(struct regs *r) {
 }
 
 void i686_timer_init(void) {
-  isr_handler_register(TIMER_VECTOR, timer_isr);
+  isr_handler_register(TIMER_INT, timer_isr);
   hal_irq_enable(TIMER_IRQ);
 }
